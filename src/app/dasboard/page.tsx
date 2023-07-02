f137c924dd73e11990cc0c02a4c0619c90cde78f
jeyson <jeysonkmguzman@gmail.com>
2023-07-01 20:58:13 -0500

    feat: prueba

--- src/app/dasboard/page.tsx
+++ src/app/dasboard/page.tsx
@@ -1,31 +1,30 @@
-import { getStatus, servicesUser } from '@/services/request.services';
-import { Board } from './components';
+// import { getStatus, servicesUser } from '@/services/request.services';
+// import { Board } from './components';
 import styles from './dasboard.module.scss';
-import { type UserGet, type StatusTicket } from '@/models';
+// import { type UserGet, type StatusTicket } from '@/models';
 
-async function getStatusTicket(): Promise<StatusTicket[]> {
-  const res = await getStatus().catch((err) => {
-    console.log(err);
-  });
+// async function getStatusTicket(): Promise<StatusTicket[]> {
+//   const res = await getStatus().catch((err) => {
+//     console.log(err);
+//   });
 
-  return await res?.json();
-}
+//   return await res?.json();
+// }
 
-async function getUser(): Promise<UserGet[]> {
-  const res = await servicesUser().catch((err) => {
-    console.log(err);
-  });
+// async function getUser(): Promise<UserGet[]> {
+//   const res = await servicesUser().catch((err) => {
+//     console.log(err);
+//   });
 
-  return await res?.json();
-}
+//   return await res?.json();
+// }
 
-async function page(): Promise<JSX.Element> {
-  const statusTicket: StatusTicket[] = await getStatusTicket();
-  const users: UserGet[] = await getUser();
+function page(): JSX.Element {
+  // const statusTicket: StatusTicket[] = await getStatusTicket();
+  // const users: UserGet[] = await getUser();
   return (
     <main className={styles.main}>
-      <Board columns={statusTicket} users={users} />
-      {/* <Form /> */}
+      {/* <Board columns={statusTicket} users={users} /> */}
     </main>
   );
 }
